c3038d369a93e3088a4bf854eb50035e1e738dac
Pranav Jindal <71919420+Pranav-github02@users.noreply.github.com>
2022-12-08 23:08:43 +0530

    changed logo

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
             <nav className="navbar navbar-expand-lg sticky-top">
                 <div className="container-fluid">
                     <NavLink to="/" className="navbar-brand">
-                        <img src={logo} alt="Jobeal" width="155" height="60" />
+                        <img src={logo} alt="Jobeal" width="250" height="65" />
                     </NavLink>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
